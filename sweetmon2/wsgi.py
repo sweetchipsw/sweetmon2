fbb483a9e2d74bf768034f5a5ffa3cba2c66649f
sweetchip <sweetchip@sweetchip.kr>
2018-05-22 23:26:30 +0900

    Modify wsgi.py to run on docker container.

--- sweetmon2/wsgi.py
+++ sweetmon2/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sweetmon2.settings")
 
 application = get_wsgi_application()
