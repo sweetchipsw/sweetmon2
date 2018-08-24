5120eb0f331e7154302ac8d2daf0f987d1adb7df
Seongwon Hyun <sweetchip@sweetchip.kr>
2018-08-24 11:49:37 +0900

    fixed minor bugs

--- sweetmon2/urls.py
+++ sweetmon2/urls.py
@@ -8,8 +8,3 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('web.urls')),
 ]
-
-
-# handler404 = 'web.views.error_page_notfound'
-# handler500 = 'web.views.error_internal_error'
-#
--- sweetmon2/wsgi.py
+++ sweetmon2/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 path = os.path.abspath(__file__+'/../..')
 if path not in sys.path:
     sys.path.append(path)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sweetmon2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sweetmon2.settings-container")
 
 application = get_wsgi_application()
